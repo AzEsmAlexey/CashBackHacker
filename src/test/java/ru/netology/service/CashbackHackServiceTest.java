bdebec6b6bc7e165417a02673cfc14ac7602d2b4
Neto.Changes <infero-sky@yandex.ru>
2023-03-30 22:20:14 +0300

    Add second branch

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void testPurchaseLessThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int purchase = 800;
+
+        int expected = 200;
+        int actual = service.remain(purchase);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testPurchaseMoreThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int purchase = 1550;
+
+        int expected = 450;
+        int actual = service.remain(purchase);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testPurchaseEquals1000() {
+        CashbackHackService service = new CashbackHackService();
+        int purchase = 1000;
+
+        int expected = 0;
+        int actual = service.remain(purchase);
+
+        assertEquals(actual, expected);
+    }
+
+
+}
